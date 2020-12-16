ec227789765cf1aae3cff9d1779cd17bc63e96ef
JD Harrington <jdharrington@instructure.com>
2020-12-16 16:56:07 -0500

    Add ENVOY_ADMIN_API

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,12 +3,13 @@
 set -e
 
 if [ "${SCUTTLE}" = "true" ]; then
+  export ENVOY_ADMIN_API="http://127.0.0.1:15000"
   export ISTIO_QUIT_API="http://127.0.0.1:15020"
-	export SCUTTLE_CMD=/usr/bin/scuttle
+  export SCUTTLE_CMD=/usr/bin/scuttle
 fi
 
 if [ "${1}" = "" ]; then
-				exec ${SCUTTLE_CMD} /bin/bash -l
+  exec ${SCUTTLE_CMD} /bin/bash -l
 else
-				exec ${SCUTTLE_CMD} "$@"
+  exec ${SCUTTLE_CMD} "$@"
 fi
