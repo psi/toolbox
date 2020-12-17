1fa85611081b665494a282a61bc924621efbfa50
JD Harrington <jdharrington@instructure.com>
2020-12-17 14:05:55 -0500

    Once more, with feeling

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ set -e
 export ENVOY_ADMIN_API="http://127.0.0.1:15000"
 export ISTIO_QUIT_API="http://127.0.0.1:15020"
 
-if curl --silent --max-time=1 ${ENVOY_ADMIN_API} > /dev/null; then
+if curl --silent --max-time 1 ${ENVOY_ADMIN_API} > /dev/null; then
   export SCUTTLE_CMD=/usr/bin/scuttle
 fi
 
